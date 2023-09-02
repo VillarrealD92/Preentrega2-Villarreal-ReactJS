a06956eeb7c088944590a5ef2072b4566e0bb6e1
Villarreal.D <villarreal.d96@gmail.com>
2023-09-02 01:01:31 -0300

    arreglo de indentacion

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,24 +3,20 @@ import {
     Routes,
     Route,
   } from "react-router-dom";
-import '../App.css';
-import NavBar from '../components/NavBar';
-import ItemListContainer from '../components/ItemListContainer';
-import ItemDetailContainer from '../components/ItemDetailContainer';
-
-
-
-
-export default function Router (){
-
+  import '../App.css';
+  import NavBar from '../components/NavBar';
+  import ItemListContainer from '../components/ItemListContainer';
+  import ItemDetailContainer from '../components/ItemDetailContainer';
+  
+  export default function Router() {
     return (
-        <BrowserRouter>
+      <BrowserRouter>
         <NavBar />
         <Routes>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route path="/categoria/:id" element={<ItemListContainer />} />
-            <Route path="/servicio/:id" element={<ItemDetailContainer />} />
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/categoria/:id" element={<ItemListContainer />} />
+          <Route path="/servicio/:id" element={<ItemDetailContainer />} />
         </Routes>
-        </BrowserRouter>
-    )
-}
+      </BrowserRouter>
+    );
+  }
